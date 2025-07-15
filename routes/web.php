162e47a42f91b43bde48867751625274e73f537e
Lil55Boo <amadoundiayepro18@gmail.com>
2025-07-15 12:15:27 +0200

    Render6

--- routes/web.php
+++ routes/web.php
@@ -11,3 +11,14 @@ use Illuminate\Support\Facades\Response;
 Route::get('/{any}', function () {
     return File::get(public_path('index.html'));
 })->where('any', '.*');
+
+use Illuminate\Support\Facades\Artisan;
+
+Route::get('/run-seeder', function () {
+    if (request()->get('key') !== 'super-secret-key') {
+        abort(403, 'Unauthorized');
+    }
+
+    Artisan::call('migrate:fresh --seed'); // pour reset et insérer les données
+    return 'Base de données réinitialisée et seeders exécutés ✅';
+});
